71331c8495928a5c18f92c10a7c3ba79530b4f9d
Dustin Richmond <drichmond@eng.ucsd.edu>
2017-12-28 07:31:58 -0800

    Fixed LRU_WAYS bug in caches

--- rtl/verilog/core/cache/riscv_dcache_core.sv
+++ rtl/verilog/core/cache/riscv_dcache_core.sv
@@ -584,7 +584,12 @@ endgenerate
     if      (!rstn               ) way_random <= 'h0;
     else if (!filling && !writing) way_random <= {way_random, way_random[19] ~^ way_random[16]};
 
-  assign fill_way_select_rnd = 1 << way_random[LRU_BITS-1:0];
+generate 
+  if (LRU_BITS == 0)
+    assign fill_way_select_rnd = 1;
+  else
+    assign fill_way_select_rnd = 1 << way_random[LRU_BITS-1:0];
+endgenerate 
 
 
   //select which way to fill (implement replacement algorithms)
